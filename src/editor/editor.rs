02069d2cec4d7517152071d827bce0826ab1b3c4
p4thie <pathiestic@gmail.com>
2023-05-28 17:28:42 +0900

    Changed colors from blue to teal

--- src/editor/editor.rs
+++ src/editor/editor.rs
@@ -2,6 +2,7 @@ use atomic_float::AtomicF32;
 use nih_plug::prelude::{util, Editor};
 use nih_plug_vizia::vizia::image::Pixel;
 use nih_plug_vizia::vizia::prelude::*;
+use nih_plug_vizia::vizia::vg::Paint;
 use nih_plug_vizia::vizia_assets;
 use nih_plug_vizia::widgets::*;
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
